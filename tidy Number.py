6ef9c78d98c7e6b72428525d6817376b6c2e9c29
rachwong <rachwong.xx@gmail.com>
2017-05-04 21:31:20 +1200

    Add files via upload

--- tidy Number.py
+++ tidy Number.py
@@ -8,17 +8,22 @@ Case = 1
 found = False
 tidynumber = 0
 
+#tidy number is where numbers are ascending. we want to find highest tidy number up to a certain given number
+#my code iterates down from certain given number, generates what the tidy number would be by sorting the numbers so they are ascending and then comparing with the actual number to see whether
+#that number is tidy.
+
 for text in file[1:integer]:
-    #print(text)
     #print("Case #" + str(Case) + ": ")#final string
     #for number in range(int(text),0,-1): #my iterator going through numbers down
 
+    #checking for number ending in 0??
     if (str(text)[-1:]) == str(0) and (str(text)[-2:]) == str(1):
         string = 1
         for i in range(len(str(text))):
             string = str(string) + str(0)
-            #print(string)
-            
+         
+        #actual code here
+        #Checks each number against a sorted version of the number to see whether tidy or not
         for number in range(int(string),0,-1):
                     #print(number)
                     ascending = "".join(sorted(str(number)))
@@ -26,6 +31,7 @@ for text in file[1:integer]:
                         if str(number) == ascending:
                             print("Case #" + str(Case) + ": " + str(number))
                             found = True
+                            
     
     elif len(str(text)) != 1:
         
@@ -35,7 +41,8 @@ for text in file[1:integer]:
                 if str(number) == ascending:
                     print("Case #" + str(Case) + ": " + str(number))
                     found = True
-        #print(text)
+        
+    #if number is length 1 it is tidy
     elif len(str(text)) == 1:
             print("Case #" + str(Case) + ": " + str(text))#final string
             found = True
